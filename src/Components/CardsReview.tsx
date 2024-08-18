c7630e24d48c06e1bed2267ce6cb787db35e4046
Mark Celoza <markangeloceloza380@gmail.com>
2024-08-18 19:59:17 +0800

    feat:updated

--- src/Components/CardsReview.tsx
+++ src/Components/CardsReview.tsx
@@ -1,9 +1,9 @@
-import Ratings from "../Components/Rating";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import Ratings from "../Components/Rating";
 
 type allCards = {
   id: number;
@@ -18,7 +18,7 @@ function CardsReview({ img, comment, name }: allCards) {
       <div className="w-80 p-3 bg-gray-100 shadow-lg shadow-gray-400">
         <div className="flex">
           <img
-            className="w-10 h-10 rounded-full mt-1 hover:w-16 hover:h-16 transition-all"
+            className="w-10 h-10 object-cover rounded-full mt-1 hover:w-16 hover:h-16 transition-all"
             src={img}
             alt=""
           />
